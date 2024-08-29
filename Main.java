28988baf138ba4502c5d8c80c98ca6bf4d53cd7f
Deepak Purohit <129038192+purohitdeepak@users.noreply.github.com>
2024-08-29 22:48:28 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Main{
+public static void main(String ar[]){
+System.out.println("this is java program");
+System.out.println("output");
+System.out.println(new Date());
+
+}
+}
