c449cdeb3010dd63fc11cfc7d8398ddc0e0bf414
Deepak Purohit <129038192+purohitdeepak@users.noreply.github.com>
2024-08-30 12:25:08 +0530

    Added Student class

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ System.out.println("this is java program");
 System.out.println("output");
 System.out.println(new Date());
 System.out.println("added new file");
+Student student=new Student();
+student.sayHello();
 }
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student{
+public Student(){
+System.out.println("creating student object");
+}
+public void sayHello(){
+System.out.println("hii i am student");
+}
+}
