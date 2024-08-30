7eeb045d386e9080fa345ad97c3336be3669e2e5
Deepak Purohit <129038192+purohitdeepak@users.noreply.github.com>
2024-08-30 12:07:51 +0530

    added new file

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public static void main(String ar[]){
 System.out.println("this is java program");
 System.out.println("output");
 System.out.println(new Date());
-
+System.out.println("added new file");
 }
 }
